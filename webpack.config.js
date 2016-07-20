eded1f170ef6d2d9076c1cbbbd0c09aff0e69f27
derekb <derek.burkgren@gmail.com>
2016-07-19 19:57:57 -0700

    This refreshes the browser on changes, but honestly it's kind of a mystery.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
+const merge = require('webpack-merge');
+const webpack = require('webpack');
 const path = require('path');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
     app: path.join(__dirname, 'app'),
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
     entry: {
         app: PATHS.app
     },
@@ -14,3 +17,25 @@ module.exports = {
         filename: 'bundle.js'
     }
 };
+
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devServer: {
+            contentBase: PATHS.build,
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+            stats: 'errors-only',
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {});
+}
